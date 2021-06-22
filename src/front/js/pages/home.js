5171440c398960bfb6da9e79d9404abdbe208875
edianibarrola <edianibarrola@gmail.com>
2021-06-22 23:22:29 +0000

    made shadwo for box

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,7 +9,16 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<div className="circle">
-				<div className="circleContent shimmer">hello</div>
+				<div className="circleContent shimmer">Hello</div>
+			</div>
+			<div className="circle">
+				<div className="circleContent shimmer">Neo</div>
+			</div>
+			<div className="circle">
+				<div className="circleContent shimmer">Morphism</div>
+			</div>
+			<div className="block">
+				<div className="blockContent shimmer">hello</div>
 			</div>
 		</div>
 	);
